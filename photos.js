d19b892cf123217c54a6d25e4c08d68bad96c4ef
liamharcombe <29879508+liamharcombe@users.noreply.github.com>
2026-08-15 08:18:57 +0000

    Update photos manifest

--- photos.js
+++ photos.js
@@ -6,5 +6,5 @@ window.tripPhotos = {
   "munich": [],
   "paris": [],
   "rome": [],
-  "vienna": ["IMG_3642 copy.jpg", "IMG_3642.jpg", "IMG_3643.jpg", "IMG_3644.jpg", "IMG_3649.jpg", "dolomites2.jpg"],
+  "vienna": [],
 };
