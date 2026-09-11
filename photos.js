7556112977f6cb0acf5d4830ea064cd2e1abeca0
liamharcombe <29879508+liamharcombe@users.noreply.github.com>
2026-09-11 14:36:53 +0000

    Update photos manifest

--- photos.js
+++ photos.js
@@ -6,5 +6,5 @@ window.tripPhotos = {
   "munich": [],
   "paris": [],
   "rome": [],
-  "vienna": ["IMG_3883.jpeg", "IMG_3898.jpeg", "IMG_3899.jpeg", "IMG_3902.jpeg", "IMG_3912.jpeg", "IMG_3914.jpeg", "IMG_3919.jpeg", "IMG_3923.jpeg", "IMG_3926.jpeg", "IMG_3932.jpeg", "IMG_3933.jpeg", "IMG_3935.jpeg", "IMG_3944.jpeg", "IMG_3947.jpeg", "IMG_3957.jpeg", "IMG_3958.jpeg", "IMG_3960.jpeg", "IMG_3980.jpeg", "IMG_3981.jpeg", "IMG_3983.jpeg", "IMG_3987.jpeg", "IMG_3992.jpeg", "IMG_7532.jpeg"],
+  "vienna": ["IMG_3883.jpeg", "IMG_3898.jpeg", "IMG_3899.jpeg", "IMG_3902.jpeg", "IMG_3912.jpeg", "IMG_3914.jpeg", "IMG_3919.jpeg", "IMG_3923.jpeg", "IMG_3926.jpeg", "IMG_3932.jpeg", "IMG_3933.jpeg", "IMG_3935.jpeg", "IMG_3944.jpeg", "IMG_3947.jpeg", "IMG_3957.jpeg", "IMG_3958.jpeg", "IMG_3960.jpeg", "IMG_3980.jpeg", "IMG_3981.jpeg", "IMG_3983.jpeg", "IMG_3987.jpeg", "IMG_3992.jpeg", "IMG_3996.jpeg", "IMG_3999.jpeg", "IMG_4002.jpeg", "IMG_4005.jpeg", "IMG_4006.jpeg", "IMG_7532.jpeg"],
 };
